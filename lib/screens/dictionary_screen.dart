f1e7b65e93e2915c2713776c7345df7298fcd7f1
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2022-05-05 21:23:19 +0700

    Add raw version dictionary

--- lib/screens/dictionary_screen.dart
+++ lib/screens/dictionary_screen.dart
@@ -1,26 +1,156 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:learning_english_flutter_app/helpers/dictionary_service.dart';
+import 'package:learning_english_flutter_app/models/dictionary_model.dart';
 import 'package:learning_english_flutter_app/models/user_model.dart';
 import 'package:provider/provider.dart';
 
-class DictionaryScreen extends StatelessWidget {
+class DictionaryScreen extends StatefulWidget {
   static const String routeName = '/dictionary_screen';
 
   const DictionaryScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    UserModel user = Provider.of<UserModel>(context);
+  State<DictionaryScreen> createState() => _DictionaryScreenState();
+}
 
+class _DictionaryScreenState extends State<DictionaryScreen> {
+  TextEditingController inputController = TextEditingController();
+
+  AudioPlayer? audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      audioPlayer = AudioPlayer();
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    audioPlayer!.dispose();
+  }
+
+  void playAudio(String music) {
+    audioPlayer!.stop();
+
+    audioPlayer!.play(music);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('THIS IS USER NAME: ${user.name.toString()}'),
-            Text('THIS IS USER UID: ${user.uid.toString()}'),
-            Text('THIS IS USER EMAIL: ${user.email.toString()}'),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Dictionary App'),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: inputController,
+                            decoration: const InputDecoration(
+                              label: Text('Search Query'),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            if (inputController.text.isNotEmpty) {
+                              setState(() {});
+                            }
+                          },
+                          icon: const Icon(Icons.search),
+                        )
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  ///FutureBuilder
+                  inputController.text.isEmpty
+                      ? const SizedBox(child: Text('Search for something'))
+                      : FutureBuilder(
+                          future: DictionaryService()
+                              .getMeaning(word: inputController.text),
+                          builder: (context,
+                              AsyncSnapshot<List<DictionaryModel>> snapshot) {
+                            print("Data $snapshot");
+                            if (snapshot.hasData) {
+                              return Expanded(
+                                child: ListView(
+                                  children: List.generate(snapshot.data!.length,
+                                      (index) {
+                                    final data = snapshot.data![index];
+                                    return Container(
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            child: ListTile(
+                                              title: Text(data.word!),
+                                              subtitle: Text(
+                                                  data.phonetics![index].text!),
+                                              trailing: IconButton(
+                                                  onPressed: () {
+                                                    final path = data
+                                                        .phonetics![index]
+                                                        .audio;
+                                                    playAudio(data.phonetics![index].audio as String);
+                                                    print('THIS IS PATH: ${data.phonetics![index].audio}');
+                                                  },
+                                                  icon: const Icon(
+                                                      Icons.audiotrack)),
+                                            ),
+                                          ),
+                                          Container(
+                                            child: ListTile(
+                                              title: Text(data
+                                                  .meanings![index]
+                                                  .definitions![index]
+                                                  .definition!),
+                                              subtitle: Text(data
+                                                  .meanings![index]
+                                                  .partOfSpeech!),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text(snapshot.error.toString());
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          },
+                        )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }

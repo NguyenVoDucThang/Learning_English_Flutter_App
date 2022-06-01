21889a3095dd6ebad43a5afd5960c7ac207842f2
Nguyễn Võ Đức Thắng <74752886+NguyenVoDucThang@users.noreply.github.com>
2022-06-01 08:59:41 +0700

    Revert "hot fix"

--- lib/widgets/dictionary_widget/custom_card.dart
+++ lib/widgets/dictionary_widget/custom_card.dart
@@ -17,34 +17,29 @@ class CustomCard extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> list = listThesaurus.map((s) => s.toString()).toList();
 
-    return Column(
-      children: [
-        const SizedBox(height: 15),
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            width: MediaQuery.of(context).size.width * 0.9,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                      color: Color.fromRGBO(96, 98, 125, 1),
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto'),
-                ),
-                const SizedBox(height: 5),
-                WrapChipWidget(listString: list,),
-              ],
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                  color: Color.fromRGBO(96, 98, 125, 1),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto'),
             ),
-          ),
+            const SizedBox(height: 5),
+            WrapChipWidget(listString: list,),
+          ],
         ),
-      ],
+      ),
     );
   }
 }

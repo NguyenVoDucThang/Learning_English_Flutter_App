8fec56c6870ca4444e9f75802904d73f6fd67c2c
Dương Tùng <19522494@gm.uit.edu.vn>
2022-06-08 14:52:27 +0700

    add menu

---
+++ lib/screens/translate_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TranslateScreen extends StatefulWidget {
+  const TranslateScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TranslateScreen> createState() => _TranslateScreenState();
+}
+
+class _TranslateScreenState extends State<TranslateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
